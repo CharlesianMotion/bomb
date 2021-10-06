0e809dceea010f4941bf3c7747c3f69567803e68
CharlesianMotion <liqianchen123@qq.com>
2021-10-07 04:45:59 +0800

    first commit

---
+++ first.go
@@ -0,0 +1,11 @@
+package bomb
+
+import "fmt"
+
+func Publicbomb() {
+	fmt.Println("Public bomb")
+}
+
+func privatebomb() {
+	fmt.Println("private bomb")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/charlesianmotion/bomb
+
+go 1.17
